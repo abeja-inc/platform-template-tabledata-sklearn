da1258a8612405386fd0b1ff9c1ed34f6dfea9e2
Kazuki Onodera <luvsic02@gmail.com>
2019-09-02 08:33:42 +0900

    Onodera work (#1)

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # platform-template-tabledata-sklearn
+## Environment options
+### DataLake
+env name|required|default|description
+--------|--------|-------|-----------
+DATALAKE_CHANNEL_ID|True|None|DataLake channel id
+DATALAKE_TRAIN_ITEM_ID|True|None|DataLake item id
+DATALAKE_TEST_ITEM_ID|False|None|DataLake item id
+
+### Features
+env name|required|default|description
+--------|--------|-------|-----------
+INPUT_FIELDS|False|None|Names of features. <br>e.g. var_1,var_2,var_3
+TARGET_FIELD|True|None|Name of target column.
+
+
+### Scikit-Learn
+env name|required|default|description
+--------|--------|-------|-----------
+CLASSIFIER|False|LinearRegression|Name of algorithm. You can choose LinearRegression, LogisticRegression, SVR and SVC.
+PARAMS|False|dict|Parameters for model. <br>e.g. max_iter=1000,n_jobs=-1
+NFOLD|False|5|To be passed to nfold.
+
---
+++ train.py
@@ -0,0 +1,151 @@
+# coding: utf-8
+# Template: 
+
+import os
+import gc
+import json
+from pathlib import Path
+from math import modf
+import pickle
+
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.svm import SVR, SVC
+from sklearn.model_selection import StratifiedKFold
+
+from abeja.datalake import Client as DatalakeClient
+
+
+
+# Configurable, but you don't need to set this. Default is "~/.abeja/.cache" in ABEJA Platform.
+ABEJA_STORAGE_DIR_PATH = os.getenv('ABEJA_STORAGE_DIR_PATH')
+ABEJA_TRAINING_RESULT_DIR = os.getenv('ABEJA_TRAINING_RESULT_DIR')
+
+DATALAKE_CHANNEL_ID = os.getenv('DATALAKE_CHANNEL_ID')
+DATALAKE_TRAIN_ITEM_ID = os.getenv('DATALAKE_TRAIN_ITEM_ID')
+DATALAKE_TEST_ITEM_ID = os.getenv('DATALAKE_TEST_ITEM_ID')
+
+# params for load
+INPUT_FIELDS = os.getenv('INPUT_FIELDS')
+TARGET_FIELD = os.getenv('TARGET_FIELD') # required
+
+if TARGET_FIELD is None:
+    raise Exception(f'TARGET is required')
+
+# params for clf
+PARAMS = os.getenv('PARAMS')
+params = {}
+if PARAMS is None:
+    pass
+elif len(PARAMS) == 0:
+    pass
+else:
+    for kv in PARAMS.split(','):
+        k, v = kv.split('=')
+        
+        try:
+            if v in ['True', 'False']:
+                params[k] = (v == 'True')
+            elif v == 'None':
+                params[k] = None
+            else:
+                # int or float
+                decimal, integer = modf(float(v))
+                if decimal == 0:
+                    params[k] = int(v)
+                else:
+                    params[k] = float(v)
+        except:
+            params[k] = v
+
+NFOLD = int(os.getenv('NFOLD', '5'))
+if NFOLD <= 1:
+    raise Exception(f'NFOLD must be larger than 2 for now. {NFOLD} was passed')
+    
+skf = StratifiedKFold(n_splits=NFOLD)
+
+CLASSIFIER = os.getenv('CLASSIFIER')
+if CLASSIFIER is None:
+    classifier = LinearRegression
+    
+elif CLASSIFIER == 'LinearRegression':
+    classifier = LinearRegression
+    
+elif CLASSIFIER == 'LogisticRegression':
+    classifier = LogisticRegression
+    
+elif CLASSIFIER == 'SVR':
+    classifier = SVR
+
+elif CLASSIFIER == 'SVC':
+    classifier = SVC
+
+
+def handler(context):
+    print('Start train handler.')
+    
+    # load train
+    datalake_client = DatalakeClient()
+    channel = datalake_client.get_channel(DATALAKE_CHANNEL_ID)
+    datalake_file = channel.get_file(DATALAKE_TRAIN_ITEM_ID)
+    datalake_file.get_content(cache=True)
+    
+    csvfile = Path(ABEJA_STORAGE_DIR_PATH, DATALAKE_CHANNEL_ID, DATALAKE_TRAIN_ITEM_ID)
+    if INPUT_FIELDS is None:
+        train = pd.read_csv(csvfile)
+    else:
+        usecols = INPUT_FIELDS.split(',')
+        train = pd.read_csv(csvfile, usecols=usecols+[TARGET_FIELD])
+    
+    y_train = train[TARGET_FIELD].values
+    cols_drop = [c for c in train.columns if train[c].dtype == 'O'] + [TARGET_FIELD]
+    train.drop(cols_drop, axis=1, inplace=True)
+    X_train = train
+    cols_train = X_train.columns.tolist()
+    del train
+    
+    models = []
+    pred = np.zeros(len(X_train))
+    for i, (train_index, valid_index) in enumerate(skf.split(X_train, y_train)):
+        model = classifier(**params)
+        model.fit(X_train.iloc[train_index], y_train[train_index])
+        pred[valid_index] = model.predict(X_train.iloc[valid_index])
+        
+        filename = os.path.join(ABEJA_TRAINING_RESULT_DIR, f'model_{i}.pkl')
+        pickle.dump(model, open(filename, 'wb'))
+        
+        models.append(model)
+    
+    di = {
+            'NFOLD': NFOLD,
+            'CLASSIFIER': CLASSIFIER,
+            'cols_train': cols_train
+            
+        }
+    skf_env = open(os.path.join(ABEJA_TRAINING_RESULT_DIR, 'skf_env.json'), 'w')
+    json.dump(di, skf_env)
+    skf_env.close()
+    
+    del X_train; gc.collect()
+    
+    # load test
+    if DATALAKE_TEST_ITEM_ID is not None:
+        datalake_client = DatalakeClient()
+        channel = datalake_client.get_channel(DATALAKE_CHANNEL_ID)
+        datalake_file = channel.get_file(DATALAKE_TEST_ITEM_ID)
+        datalake_file.get_content(cache=True)
+        
+        csvfile = Path(ABEJA_STORAGE_DIR_PATH, DATALAKE_CHANNEL_ID, DATALAKE_TEST_ITEM_ID)
+        X_test = pd.read_csv(csvfile, usecols=cols_train)[cols_train]
+        
+        pred = np.zeros(len(X_test))
+        for model in models:
+            pred += model.predict(X_test)
+        pred /= len(models)
+        
+        print(pred)
+    
+    
+    return
+

1f597896432586839a5a8db01a3dfed4b239718c
keigohtr <keigoht@gmail.com>
2019-10-03 09:42:50 +0900

    Use numpy

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
-import math
+import numpy as np
 
 from sklearn.metrics import mean_squared_error
 
 
 def inverse_root_mean_squared_error(gold, pred):
-    return 1.0/(math.sqrt(mean_squared_error(gold, pred)))
+    return 1.0/(np.sqrt(mean_squared_error(gold, pred)))
